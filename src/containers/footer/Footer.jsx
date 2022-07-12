a8d5198e8f5c8636ee3a191a138993394e878c36
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-13 00:44:58 +0200

    add: footer content & image

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./footer.scss";
 
+import footerLeaves from "../../assets/footer_leaves.png";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="grid place-items-center relative">
+      <div>
+        <button className="btn btn-primary">Napisz do nas</button>
+      </div>
+      <div className="absolute">
+        <img src={footerLeaves} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
