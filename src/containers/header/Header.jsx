c98ab24792529a86e49bf3d9cbc86fde5ddaaf43
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-14 19:17:19 +0200

    update: header responsivness

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,21 +5,23 @@ import headerPhoto from "../../assets/header_photo.png";
 
 const Header = () => {
   return (
-    <div className="px-10 text-center block md:grid grid-cols-2 gap-10">
-      <div className="mx-auto">
-        <img src={headerPhoto} className="" alt="" />
+    <section className="section-mt section-mb">
+      <div className="px-10 block md:grid grid-cols-12 gap-5 md:gap-10 lg:gap-16 xl:gap-20">
+        <div className="col-start-2 col-end-6 ">
+          <img src={headerPhoto} className="w-4/6 md:w-full mx-0 md:mx-auto" />
+        </div>
+        <div className="text-left col-start-6 col-end-12 mx-auto mt-5 md:mt-5 lg:mt-10 xl:mt-20">
+          <h1 className="font-serif">
+            Najlepsze <b>meble na wymiar</b>
+          </h1>
+          <h2>
+            Projektujemy nowoczesne i praktyczne kuchnie, garderoby, wnęki oraz
+            przestrzenie biurowe zarówno dla klientów indywidualnych jak i firm
+            czy urzędów.
+          </h2>
+        </div>
       </div>
-      <div className="text-left mx-auto mt-20">
-        <h1 className="font-serif">
-          Najlepsze <b>meble na wymiar</b>
-        </h1>
-        <h2>
-          Projektujemy nowoczesne i praktyczne kuchnie, garderoby, wnęki oraz
-          przestrzenie biurowe zarówno dla klientów indywidualnych jak i firm
-          czy urzędów.
-        </h2>
-      </div>
-    </div>
+    </section>
   );
 };
 
