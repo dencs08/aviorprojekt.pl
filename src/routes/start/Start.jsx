9efb4f7ce6bb83325b97ec7b5c69ef631bcfd105
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-15 10:06:57 +0200

    add: project & start routes

---
+++ src/routes/start/Start.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  Header,
+  OurWorks,
+  WhyUs,
+  KitchenAssembly,
+  OwnConfiguration,
+  Offer,
+} from "../../containers";
+
+const Start = () => {
+  return (
+    <div>
+      <Header />
+      <OurWorks />
+      <WhyUs />
+      <KitchenAssembly />
+      <OwnConfiguration />
+      <Offer />
+    </div>
+  );
+};
+
+export default Start;
