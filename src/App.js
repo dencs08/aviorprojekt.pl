79700ba67a42d1b74d2151bd841720acd1a83eb1
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-15 10:08:29 +0200

    update: App.js for react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { Navbar } from './components'
-import { Header, Footer } from './containers'
+import { Footer } from './containers'
+import { Start, Projects } from './routes'
 import "./scss/app.scss"
 
 const App = () => {
     return (
+
         <div className="App">
-            <div>
-                <Navbar />
-                <Header />
+            <Navbar />
+            <div className="content">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Start />} />
+                        <Route path="/start" element={<Start />} />
+                        <Route path="/projekty" element={<Projects />} />
+                    </Routes>
+                </BrowserRouter>
+
             </div>
             <Footer />
         </div>
+
     )
 }
 
