6ebf0d4d62c3a9163f189f14c71d2c13ecfd04b9
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-16 17:16:39 +0200

    add: WhyUsFeature component

---
+++ src/components/why_us/WhyUsFeature.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WhyUsFeature = ({ title, text }) => {
+  return (
+    <div>
+      <h4 className="font-sans">{title}</h4>
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default WhyUsFeature;
