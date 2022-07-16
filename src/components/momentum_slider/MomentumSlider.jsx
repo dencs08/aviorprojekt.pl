5e6671099da317ee770db647e5938fad1dadcda3
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-16 17:16:21 +0200

    add: momentum slider component

---
+++ src/components/momentum_slider/MomentumSlider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./momentum_slider.scss";
+
+const MomentumSlider = () => {
+  return (
+    <section>
+      <h2>Slider</h2>
+    </section>
+  );
+};
+
+export default MomentumSlider;
