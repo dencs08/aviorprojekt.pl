0b8ddc22dddd334910c6f992a36639a0dd3334ab
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-16 17:16:06 +0200

    update: components index.js with WhyUsFeature and MomentumSlider

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Navbar } from './navbar/Navbar'
 export { default as Card } from './card/Card'
+export { default as WhyUsFeature } from './why_us/WhyUsFeature'
+export { default as MomentumSlider } from './momentum_slider/MomentumSlider'
