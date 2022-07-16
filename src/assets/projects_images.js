5788bf3e092e1dd0a020b2e26480c0518a255135
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-16 21:13:34 +0200

    update: MomentumSlider content

---
+++ src/assets/projects_images.js
@@ -0,0 +1,12 @@
+import image1 from './project/3.jpg'
+import image2 from './project/kitchen1.jpg'
+import image3 from './project/wardrobe1.jpg'
+import image4 from './project/FOTO5.jpg'
+import image5 from './project/FOTO2.jpg'
+import image6 from './project/FOTO3.jpg'
+import image7 from './project/foto6.jpg'
+import image8 from './project/1.jpg'
+import image9 from './project/m_stepien_1.jpg'
+import image10 from './project/2.jpg'
+
+export default [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10]
--- src/components/momentum_slider/MomentumSlider.jsx
+++ src/components/momentum_slider/MomentumSlider.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import { useRef, useEffect, useState } from "react";
+
+import { motion } from "framer-motion";
+
 import "./momentum_slider.scss";
 
+import images from "../../assets/projects_images";
+
 const MomentumSlider = () => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
   return (
     <section>
-      <h2>Slider</h2>
+      <motion.div
+        ref={carousel}
+        className="carousel cursor-grab overflow-hidden"
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel flex"
+        >
+          {images.map((image) => {
+            return (
+              <motion.div
+                className="h-[60vh] min-w-[44vw] p-10 image-carousel-wrapper"
+                key={image}
+              >
+                <img
+                  src={image}
+                  className="h-full rounded-xl object-cover object-center"
+                  alt=""
+                  draggable="false"
+                />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
     </section>
   );
 };
