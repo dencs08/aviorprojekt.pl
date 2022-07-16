9563143f5db32399219e0840ab0f108c0faf9b73
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-16 17:17:38 +0200

    update: Projects route content

--- src/routes/projects/Projects.jsx
+++ src/routes/projects/Projects.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { MomentumSlider } from "../../components";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="section-mt section-mb">
+      <div className="content-wrapper text-center">
+        <h1 className="font-serif">Zrealizowane projekty</h1>
+      </div>
+      <MomentumSlider />
+    </div>
+  );
 };
 
 export default Projects;
