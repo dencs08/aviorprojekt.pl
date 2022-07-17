630a69c9f709a7799307f059668901043732f655
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-17 23:47:28 +0200

    update: WhyUsFeature component with icon prop

--- src/components/why_us/WhyUsFeature.jsx
+++ src/components/why_us/WhyUsFeature.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const WhyUsFeature = ({ title, text }) => {
+const WhyUsFeature = ({ title, text, icon }) => {
   return (
     <div>
-      <h4 className="font-sans">{title}</h4>
+      <h4 className="font-sans flex mb-2">
+        <span className="mr-3">{icon}</span>
+        <span className="mt-1">{title}</span>
+      </h4>
       <p>{text}</p>
     </div>
   );
