532f4683360a4c99498a4ceded19458abb76a536
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-07-20 12:20:28 +0200

    add: useLocomotiveScroll hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import useLocoScroll from './hooks/useLocoScroll'
 
 import { Navbar } from './components'
 import { Footer } from './containers'
@@ -7,22 +9,72 @@ import { Start, Projects } from './routes'
 import "./scss/app.scss"
 
 const App = () => {
+    const ref = useRef(null);
+    // const [preloader, setPreload] = useState(true);
+
+    useLocoScroll();
+
+    useEffect(() => {
+        if (ref) {
+            if (typeof window === "undefined" || !window.document) {
+                return;
+            }
+        }
+    }, []);
+
+    const [timer, setTimer] = React.useState(1);
+
+    const id = React.useRef(null);
+
+    const clear = () => {
+        window.clearInterval(id.current);
+        // setPreload(false);
+    };
+
+    React.useEffect(() => {
+        id.current = window.setInterval(() => {
+            setTimer((time) => time - 1);
+        }, 1000);
+        return () => clear();
+    }, []);
+
+    React.useEffect(() => {
+        if (timer === 0) {
+            clear();
+        }
+    }, [timer]);
+
+    if (typeof window === "undefined" || !window.document) {
+        return null;
+    }
     return (
 
-        <div className="App">
-            <Navbar />
-            <div className="content">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Start />} />
-                        <Route path="/start" element={<Start />} />
-                        <Route path="/projekty" element={<Projects />} />
-                    </Routes>
-                </BrowserRouter>
+        <>
+            {/* <CustomCursor /> */}
+            {/* {preloader ? (
+                <div className="loader-wrapper absolute top-[50%] left-[50%] text-center ">
+                    <h1>Aviorprojekt</h1>
+                    <h2>Urządź się tak, jak chcesz.</h2>
+                </div>
+            ) : ( */}
 
+            <div className="App main-container" id="main-container"
+                data-scroll-container
+                ref={ref}>
+                <div className="content">
+                    <Navbar />
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Start />} />
+                            <Route path="/start" element={<Start />} />
+                            <Route path="/projekty" element={<Projects />} />
+                        </Routes>
+                    </BrowserRouter>
+                    <Footer />
+                </div>
             </div>
-            <Footer />
-        </div>
+
+        </>
 
     )
 }
