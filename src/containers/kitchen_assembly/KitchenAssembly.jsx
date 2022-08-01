bd3394b0abef63887f6dcd382c3ab96d8d74252c
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-08-01 22:39:20 +0200

    add: kitchenAssembly.jsx gsap animation

--- src/containers/kitchen_assembly/KitchenAssembly.jsx
+++ src/containers/kitchen_assembly/KitchenAssembly.jsx
@@ -5,12 +5,44 @@ import kitchen from "../../assets/kitchen/kitchen_bg.png";
 import plant from "../../assets/kitchen/plant.png";
 import table from "../../assets/kitchen/kitchen_table.png";
 
+import gsap from "gsap";
+
 const KitchenAssembly = () => {
+  gsap.fromTo(
+    ".kitchen",
+    { y: "-20%" },
+    { y: "22%", duration: 1.5, delay: 0.5, ease: "expo" }
+  );
+  gsap.fromTo(
+    ".table",
+    { x: "-50%" },
+    { x: "-25%", duration: 2, delay: 0.55, ease: "expo" }
+  );
+  gsap.fromTo(
+    ".plant",
+    { x: "50%" },
+    { x: "-100%", duration: 2.5, delay: 0.61, ease: "expo" }
+  );
+  gsap.fromTo(
+    ".kitchen",
+    { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" },
+    {
+      clipPath: "polygon(7% 0%, 82% 0%, 82% 100%, 7% 100%)",
+      duration: 1.5,
+      delay: 1.1,
+      ease: "expo",
+    }
+  );
+
   return (
     <section id="kitchenAssembly" className="section-mt-xl section-mb">
       <div className="content-wrapper">
         <div className="block md:grid grid-cols-12">
-          <div className="col-span-3 flex flex-col justify-center">
+          <div
+            data-scroll
+            data-scroll-speed="3"
+            className="col-span-3 flex flex-col justify-center"
+          >
             <h5 className="font-sans">od</h5>
             <h4 className="font-serif">2012</h4>
             <p>
