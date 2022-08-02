8f6808e2507e9caa08bc007e0f6075c9981a3300
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-08-02 15:01:18 +0200

    update: ourWorks.jsx responsivness

--- src/containers/our_works/OurWorks.jsx
+++ src/containers/our_works/OurWorks.jsx
@@ -30,7 +30,9 @@ const OurWorks = () => {
           <img src={flower3} className="opacity-10 rotate-[-30deg]" alt="" />
         </div>
 
-        <h3 className="font-sans mb-20">Nasze prace</h3>
+        <h3 className="font-sans mb-8 md:mb-12 lg:mb-16 xl:mb-20">
+          Nasze prace
+        </h3>
         <div>
           <div className="">
             <img className="mx-auto" src={center} alt="" />
@@ -40,7 +42,7 @@ const OurWorks = () => {
             data-scroll-speed="3"
             data-scroll-direction="horizontal"
             data-scroll-delay="0.1"
-            className="absolute top-96 left-80 w-1/5"
+            className="absolute top-96 left-10 sm:left-16 md:left-32 lg:left-52 xl:left-80 w-1/5 hidden lg:block"
           >
             <img src={topLeft} alt="" />
           </div>
@@ -49,7 +51,7 @@ const OurWorks = () => {
             data-scroll-speed="-3.25"
             data-scroll-direction="horizontal"
             data-scroll-delay="0.1"
-            className="absolute top-72 right-80 w-1/5"
+            className="absolute top-72 right-10 sm:right-16 md:right-32 lg:right-52 xl:right-80 w-1/5 hidden lg:block"
           >
             <img src={topRight} alt="" />
           </div>
@@ -58,7 +60,7 @@ const OurWorks = () => {
             data-scroll-speed="2"
             data-scroll-direction="horizontal"
             data-scroll-delay="0.1"
-            className="absolute bottom-[-10%] left-60 w-1/5"
+            className="absolute bottom-[-10%] left-10 sm:left-16 md:left-32 lg:left-60 w-1/5 hidden lg:block"
           >
             <img src={bottomLeft} alt="" />
           </div>
@@ -67,7 +69,7 @@ const OurWorks = () => {
             data-scroll-speed="-1"
             data-scroll-direction="horizontal"
             data-scroll-delay="0.1"
-            className="absolute bottom-[-5%] right-72 w-1/5"
+            className="absolute bottom-[-5%] right-10 sm:right-16 md:right-40 lg:right-72 w-1/5 hidden lg:block"
           >
             <img src={bottomRight} alt="" />
           </div>
