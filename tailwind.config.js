df0fa8f255a79fddcf980f443be947cfee01da9f
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-08-14 17:00:28 +0200

    add: 2xs & xs tailwind breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+    screens: {
+      '2xs': '400px',
+      'xs': '500px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
