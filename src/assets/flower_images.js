ac7d7f9edbfd757fdb1720861cdf82da806ff804
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-08-17 01:06:45 +0200

    add: uncompressed version of svgs

--- src/assets/flower_images.js
+++ src/assets/flower_images.js
@@ -1,5 +1,5 @@
-import flower1 from "./flowers/1.svg";
-import flower2 from "./flowers/2.svg";
-import flower3 from "./flowers/3.svg";
+import flower1 from "./flowers/uncompressed/1.svg";
+import flower2 from "./flowers/uncompressed/2.svg";
+import flower3 from "./flowers/uncompressed/3.svg";
 
 export { flower1, flower2, flower3 }
