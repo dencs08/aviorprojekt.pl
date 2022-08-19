7ba69b5336208dc122bb7a403099f249600df912
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-08-19 12:55:17 +0200

    add: prallax effect on the Projects.jsx

--- src/routes/projects/Projects.jsx
+++ src/routes/projects/Projects.jsx
@@ -3,12 +3,14 @@ import { MomentumSlider } from "../../components";
 
 const Projects = () => {
   return (
-    <div className="section-mt section-mb">
-      <div className="content-wrapper text-center">
-        <h1 className="font-serif">Zrealizowane projekty</h1>
+    <section data-scroll-section className="section-mt section-mb">
+      <div data-scroll data-scroll-speed="-6">
+        <div className="content-wrapper text-center">
+          <h1 className="font-serif">Zrealizowane projekty</h1>
+        </div>
+        <MomentumSlider />
       </div>
-      <MomentumSlider />
-    </div>
+    </section>
   );
 };
 
