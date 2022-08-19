1ce6f56525663cd3240163363b43e45e0ed76dfa
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-08-19 12:52:02 +0200

    update: flowers.svg are now flowers.png

--- src/assets/flower_images.js
+++ src/assets/flower_images.js
@@ -1,5 +1,5 @@
-import flower1 from "./flowers/uncompressed/1.svg";
-import flower2 from "./flowers/uncompressed/2.svg";
-import flower3 from "./flowers/uncompressed/3.svg";
+import flower1 from "./flowers/uncompressed/1.png";
+import flower2 from "./flowers/uncompressed/2.png";
+import flower3 from "./flowers/uncompressed/3.png";
 
 export { flower1, flower2, flower3 }
