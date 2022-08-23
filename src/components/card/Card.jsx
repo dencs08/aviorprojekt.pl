c21fdec33c9c5477ae8f895154699713ff9546f0
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-08-23 15:10:54 +0200

    update: Card.jsx is now using gsap animations

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import { BsCheck2 } from "react-icons/bs";
 import "./card.scss";
 
+import gsap from "gsap";
+
 const Card = ({ title, features }) => {
+  const onEnter = ({ currentTarget }) => {
+    gsap.to(currentTarget, {
+      backgroundColor: "#ffffff",
+      scale: 1.05,
+      boxShadow: "0 0 12.5px #0000005d",
+      ease: "expo",
+      duration: 0.5,
+    });
+  };
+
+  const onLeave = ({ currentTarget }) => {
+    gsap.to(currentTarget, {
+      backgroundColor: "#fcfcff",
+      scale: 1,
+      boxShadow: "0 0 15px #0000002f",
+      ease: "expo",
+      duration: 0.5,
+    });
+  };
+
   const data = features;
   const feautresList = data.map((feature) => (
     <li className="my-3 flex flex-row flex-initial" key={feature}>
@@ -12,7 +34,11 @@ const Card = ({ title, features }) => {
   ));
 
   return (
-    <div className="card px-10 py-8 my-10 lg:my-0 mx-auto w-full sm:w-4/6 lg:w-full min-w-[100px] lg:min-w-[250px] shadow-md rounded-2xl">
+    <div
+      onMouseEnter={onEnter}
+      onMouseLeave={onLeave}
+      className="card px-10 py-8 my-10 lg:my-0 mx-auto w-full sm:w-4/6 lg:w-full min-w-[100px] lg:min-w-[250px] shadow-md rounded-2xl"
+    >
       <div className="card-header text-center font-serif uppercase font-bold">
         {title}
       </div>
